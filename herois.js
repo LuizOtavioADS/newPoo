145e7f700412b6504de68cb962465c719dc82a14
LuizOtavioADS <luizo7avio7@gmail.com>
2023-12-09 23:12:53 -0300

    commit1
    one

---
+++ herois.js
@@ -0,0 +1,37 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'usou ataque desconhecido';
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso:
+const heroi1 = new Heroi('Herói 1', 25, 'mago');
+const heroi2 = new Heroi('Herói 2', 30, 'guerreiro');
+
+heroi1.atacar();
+heroi2.atacar();
